fff90b3635e5ab92bb9d21d0dd3be3c4b53ffeb7
binking338 <binking338@163.com>
2015-04-16 22:48:53 +0800

    +

---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var idcardno = DotNETX.Identify.IDCard.IdCardNo.Parse("330881198701290034");
+            Assert.AreEqual("浙江省江山市", idcardno.County);
+            Assert.AreEqual(new DateTime(1987, 1, 29), idcardno.Birthday);
+        }
+    }
+}
